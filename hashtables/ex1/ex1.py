0119ee35c42ca51453f535cb1d82743e0069a53e
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2020-03-13 09:42:09 -0700

    basically finished exercise 1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -7,12 +7,24 @@ from hashtables import (HashTable,
 
 
 def get_indices_of_item_weights(weights, length, limit):
-    ht = HashTable(16)
+    # Should this be length instead of magic number?
+    ht = HashTable(length)
 
-    """
-    YOUR CODE HERE
-    """
+    for i in range(len(weights) - 1):
+        hash_table_insert(ht, weights[i], i)
+    
+    for weight in weights:
+        index1 = hash_table_retrieve(ht, weight)
+        print(f"Index 1: {index1}")
+        index2 = hash_table_retrieve(ht, limit-weight)
+        print(f"Index 2: {index2}")
 
+        if index2 is not None:
+            if index1 >= index2:
+                return (index1, index2)
+            else:
+                return (index2, index1)
+    
     return None
 
 
